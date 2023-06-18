104ddeff18a491b2acab06cb884aea43a5a17f07
chanan-ru <chananchida.ru@gmail.com>
2023-06-18 14:53:48 +1000

    Update NumberOfEvents.js

--- src/NumberOfEvents.js
+++ src/NumberOfEvents.js
@@ -7,17 +7,26 @@ class NumberOfEvents extends Component {
 
 
     handleInputChanged = (event) => {
+        // const enteredEventnumber = prompt('Please enter Events number between 1 - 32');
         const value = event.target.value;
         this.props.updateEvents(null, value);
         this.setState({
             query: value
         });
+
+        if (value < 1 || value > 32) {
+            alert("Please enter Events number between 1 - 32");
+        }
     }
 
     render() {
         return (
             <div>
-                <span>Number of Events: </span><input className="numberOfEvents" type="number" min={1}
+                <span>Number of Events: </span>
+                <input
+                    className="numberOfEvents"
+                    type="number"
+                    min={1}
                     max={32}
                     value={this.state.query}
                     onChange={this.handleInputChanged}
