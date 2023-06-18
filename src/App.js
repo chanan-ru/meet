56d0455e3780f3c5309918510a06d2002a7ff1c0
chanan-ru <chananchida.ru@gmail.com>
2023-06-18 14:53:34 +1000

    Updated updateEvents function

--- src/App.js
+++ src/App.js
@@ -6,28 +6,23 @@ import CitySearch from './CitySearch';
 import NumberOfEvents from './NumberOfEvents';
 import { extractLocations, getEvents } from './api';
 
+
+
 class App extends Component {
   state = {
     events: [],
-    locations: []
-  }
-
-  updateEvents = (location) => {
-    getEvents().then((events) => {
-      const locationEvents = (location === 'all') ?
-        events :
-        events.filter((event) => event.location === location);
-      this.setState({
-        events: locationEvents
-      });
-    });
+    locations: [],
+    selectedLocation: 'all',
+    numberOfEvents: 32
   }
 
   componentDidMount() {
     this.mounted = true;
     getEvents().then((events) => {
       if (this.mounted) {
-        this.setState({ events, locations: extractLocations(events) });
+        this.setState({
+          events, locations: extractLocations(events)
+        });
       }
     });
   }
@@ -36,11 +31,39 @@ class App extends Component {
     this.mounted = false;
   }
 
+
+  updateEvents = (location, eventCount) => {
+    const { selectedLocation } = this.state;
+    if (location) {
+      getEvents().then((events) => {
+        const locationEvents = (location === 'all') ?
+          events : events.filter((event) => event.location === location);
+        const shownEvents = locationEvents.slice(0, eventCount);
+        this.setState({
+          events: shownEvents,
+          selectedLocation: location,
+          numberOfEvents: eventCount
+        });
+      });
+    } else {
+      getEvents().then((events) => {
+        const locationEvents = (selectedLocation === 'all') ?
+          events :
+          events.filter((event) => event.location === selectedLocation);
+        const shownEvents = locationEvents.slice(0, eventCount);
+        this.setState({
+          events: shownEvents,
+          numberOfEvents: eventCount
+        });
+      })
+    }
+  }
+
   render() {
     return (
       <div className="App">
         <CitySearch locations={this.state.locations} updateEvents={this.updateEvents} /> {/* pass locations state to CitySearch as a prop of locations */}
-        <NumberOfEvents />
+        <NumberOfEvents numberOfEvents={this.state.numberOfEvents} updateEvents={this.updateEvents} />
         <EventList events={this.state.events} /> {/* pass events state to EventList as a prop of events */}
 
       </div>
